d7e76fc0b49259997d7930437da4a3762a5922d2
Yuut <ingride.reis.b@gmail.com>
2022-08-13 17:22:24 -0300

    :construction: feat: erro no requisito 13

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+import getRandomInt from './utils';
+
+const player1 = new Character('Yuut');
+for (let up = getRandomInt(5, 15); up > 0; up -= 1) {
+  player1.levelUp();
+}
+const player2 = new Character('Ark anukai');
+const player3 = new Character('Mafeu');
+const monster1 = new Monster();
+const monster2 = new Dragon();
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]): void {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+}
+export {
+  Character,
+  player1, player2, player3, monster1, monster2, pvp, pve, runBattles,
+};
